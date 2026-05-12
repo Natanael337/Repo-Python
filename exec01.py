3aa88636d8b719ce200f04b6d82a546071226c39
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-05-12 11:11:28 -0300

    exec01.py

---
+++ exec01.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite o Primeiro Número: "))
+n2 = float(input("Digite o Segundo Número: "))
+
+
+print("Soma: ", n1 + n2)
+print("Subtração: ", n1 - n2)
+print("Multiplicação: ", n1 * n2)
+print("Divisão: ", n1 / n2)
